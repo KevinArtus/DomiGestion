383bc0767c7d530b922b1d5ed9e5a2b78cbc6ef2
Kevin Artus <artus.kevin@outlook.fr>
2019-07-31 15:53:17 +0200

    CRUD pour l'entité Reunion

---
+++ src/Form/ReunionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reunion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReunionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('prenom')
+            ->add('nom')
+            ->add('sexe')
+            ->add('adresse')
+            ->add('codePostale')
+            ->add('ville')
+            ->add('fixe')
+            ->add('portable')
+            ->add('email')
+            ->add('anniversaire')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reunion::class,
+        ]);
+    }
+}
