8755ba644bd6705e09d3f723e66db5c7b1e1cfb3
Kevin Artus <artus.kevin@outlook.fr>
2019-09-19 12:20:43 +0200

    Auto generation des crud reunion et commande

---
+++ src/Form/ReunionType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reunion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReunionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('montantHt')
+            ->add('montantTTC')
+            ->add('hote')
+            ->add('participants')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reunion::class,
+        ]);
+    }
+}
